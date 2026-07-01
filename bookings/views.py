a8f859b9f00a5009baf84363f975aa2f173921ef
Jake <yakovturnerpolt@gmail.com>
2026-07-01 17:47:30 +0800

    Added views to handle get and post requests for make_booking

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Booking
+from .forms import BookingForm
 
-# Create your views here.
+def make_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            resource = form.cleaned_data['resource']
+            start_time = form.cleaned_data['start_time']
+            end_time = form.cleaned_data['end_time']
+
+            conflicts = Booking.get_conflicts(resource, start_time, end_time)
+
+            if conflicts.exists():
+                return render(request, 'make_booking.html', {
+                    'form': form,
+                    'conflicts': conflicts
+                })
+            else:
+                form.save()
+                return render(request, 'make_booking.html', {
+                    'form': BookingForm(),
+                    'success': True
+                })
+    else:
+        form = BookingForm()
+        return render(request, 'make_booking.html', {'form': form})
